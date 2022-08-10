b8c881ed2b086d2522ef9c13d90cc20230bd43e5
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2022-08-10 14:13:58 -0700

    modified:   SynchroEnablementPowershellScripts.psm1

--- SynchroEnablementPowershellScripts.psm1
+++ SynchroEnablementPowershellScripts.psm1
@@ -1,4 +1,4 @@
-Function Get-HealthAndStatus {
+function Get-HealthAndStatus {
 
         [CmdletBinding()]
         
@@ -14,7 +14,7 @@ Function Get-HealthAndStatus {
 
                 [Parameter(Mandatory=$true)]
                 [string]
-                $TransferDirectoryPath
+                $PathToTransferDirectory
 
         )
         
@@ -50,22 +50,27 @@ Function Get-HealthAndStatus {
                                 }
 
                         }
+
+                Exit-PSSession
+
         }
         
         end {
+
                 [PSCustomObject]@{
                         Client = $ClientHealthAndStatus;
                         Server = $ServerHealthAndStatus
                 }
+                
         }
 }
-Export-ModuleMember -Function 'Get-HealthAndStatus'                     
+Export-ModuleMember -Function Get-HealthAndStatus                     
 
 
 
 
 
-Function Write-ToTransferDirectory {
+function Write-ToTransferDirectory {
         
         [CmdletBinding()]
     
@@ -84,7 +89,7 @@ Function Write-ToTransferDirectory {
 
                 [Parameter(Mandatory=$true)]
                 [string]
-                $TransferDirectoryPath,
+                $PathToTransferDirectory,
                 
                 [string]
                 $ANSIColorCodes = '\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]'           
@@ -93,46 +98,192 @@ Function Write-ToTransferDirectory {
         
         begin {
 
-                $Deliverables = @("Client Info", "Client Logs", "Server Info", "Server Logs")   
                 $Session = New-PSSession -UserName $UserName -HostName $HostName
+
+                $Deliverables = @(
+                        "Client Info",
+                        "Client Logs",
+                        "Server Info",
+                        "Server Logs"
+                )   
+        }
+        
+        process {
+
+                Invoke-Command `
+                        -Session $Session `
+                        -ArgumentList $HealthAndStatus,$Deliverables,$PathToTransferDirectory,$ANSIColorCodes `
+                        -ScriptBlock {
+
+                                Param(
+                                        [PSCustomObject]$HealthAndStatus, 
+                                        [System.Object[]]$Deliverables,
+                                        $PathToTransferDirectory,
+                                        $ANSIColorCodes
+                                )
+
+
+                                If( -not (Test-Path $PathToTransferDirectory) ){
+                                        New-Item -ItemType Directory $PathToTransferDirectory
+                                }
+
+
+                                $HealthAndStatus.Client.SystemInfo | Out-File "$PathToTransferDirectory\Client Info" -Encoding ASCII
+                                $HealthAndStatus.Client.LogList    | Out-File "$PathToTransferDirectory\Client Logs" -Encoding ASCII
+                                $HealthAndStatus.Server.SystemInfo | Out-File "$PathToTransferDirectory\Server Info" -Encoding ASCII
+                                $HealthAndStatus.Server.LogList    | Out-File "$PathToTransferDirectory\Server Logs" -Encoding ASCII
+
+
+                                $Deliverables | ForEach-Object -Process {
+
+                                        (Get-Content "$PathToTransferDirectory\$_")  `
+                                                -replace $ANSIColorCodes, '' | 
+                                                        Out-File "$PathToTransferDirectory\$_" -Encoding ASCII 
+
+                                }
+
+                        }
+
+                        
+                Exit-PSSession
+
+        }
+        
+        end {
+                
+        }
+
+}
+Export-ModuleMember -Function Write-ToTransferDirectory
+
+
+
+
+function Compare-LastWriteTimes {
+
+        [CmdletBinding()]
+        
+        param (
+        
+                [Parameter(Mandatory=$true)]
+                [string]
+                $UserName,
+
+                [Parameter(Mandatory=$true)]
+                [string]
+                $HostName,
+
+                [Parameter(Mandatory=$true)]
+                [string]
+                $PathToTransferDirectory,
+                
+                [Parameter(Mandatory=$true)]
+                [string]
+                $PathToFileDemonstratingSystemActivity
+      
+        )
+        
+        begin {
+                $Session = New-PSSession -UserName $UserName -HostName $HostName                
+        }
+        
+        process {
+                
+                Invoke-Command `
+                        -Session $Session `
+                        -ArgumentList $PathToTransferDirectory,$PathToFileDemonstratingSystemActivity `
+                        -OutVariable MostRecentlyModified `
+                        -ScriptBlock {
+
+                                Param($PathToTransferDirectory,$PathToFileDemonstratingSystemActivity)
+                                
+                                $LastTransferDirectoryWriteTime = 
+                                        (Get-ChildItem "$PathToTransferDirectory")[0].LastWriteTime
+                                
+
+                                $LastActivityWriteTime = 
+                                        (Get-ChildItem "$PathToFileDemonstratingSystemActivity").LastWriteTime
+                        
+
+                                (
+                                        ($LastTransferDirectoryWriteTime -lt $LastActivityWriteTime) `
+                                        ? `
+                                        "Activity Probe" `
+                                        : `
+                                        "Transfer Directory" `
+                                )
+
+                        }
+
+        }
+                
+        end {           
+                Exit-PSSession  
+        }
+
+}
+Export-ModuleMember -Function Compare-LastWriteTimes
+
+
+
+
+function Update-HealthAndStatus {
+
+        [CmdletBinding()]
+
+        param (
+                
+                [Parameter(ValueFromPipeline=$true)]
+                [string]
+                $MostRecentlyModified,
+        
+                [Parameter(Mandatory=$true)]
+                [string]
+                $UserName,
+
+                [Parameter(Mandatory=$true)]
+                [string]
+                $HostName,
+
+                [Parameter(Mandatory=$true)]
+                [string]
+                $PathToTransferDirectory,
+                
+                [Parameter(Mandatory=$true)]
+                [string]
+                $PathToFileDemonstratingSystemActivity
+      
+        )
+        
+        begin {
+                $Session = New-PSSession -UserName $UserName -HostName $HostName 
+
+                $ActivityHealthAndStatus = 
+                
+                        ($MostRecentlyModified -match 'Transfer Directory') `
+                        ? `
+                        "As of $(Get-Date), there has been NO additional activity observed." `
+                        : `
+                        "Recent System Activity observed on $(Get-Date)"
 
         }
         
         process {
+                
                 Invoke-Command `
                         -Session $Session `
-                        -ArgumentList $HealthAndStatus,$TransferDirectoryPath `
-                        -OutVariable ServerHealthAndStatus `
+                        -ArgumentList $PathToTransferDirectory, $ActivityHealthAndStatus `
                         -ScriptBlock {
-
-                                Param([PSCustomObject]$HealthAndStatus, $TransferDirectoryPath)
-
-                                $HealthAndStatus.Client.SystemInfo | Out-File "$TransferDirectoryPath\Client Info" -Encoding ASCII
-                                $HealthAndStatus.Client.LogList    | Out-File "$TransferDirectoryPath\Client Logs" -Encoding ASCII
-                                $HealthAndStatus.Server.SystemInfo | Out-File "$TransferDirectoryPath\Server Info" -Encoding ASCII
-                                $HealthAndStatus.Server.LogList    | Out-File "$TransferDirectoryPath\Server Logs" -Encoding ASCII
-
+                                Param($PathToTransferDirectory, $ActivityHealthAndStatus)
+                                $PathToActivityFile = "$PathToTransferDirectory\System Activity"
+                                
+                                $ActivityHealthAndStatus | Out-File $PathToActivityFile -Append
                         }
+
         }
         
         end {
-                Invoke-Command `
-                        -Session $Session `
-                        -ArgumentList $Deliverables,$TransferDirectoryPath,$ANSIColorCodes `
-                        -OutVariable ServerHealthAndStatus `
-                        -ScriptBlock {
-
-                                Param([System.Object[]]$Deliverables, $TransferDirectoryPath, $ANSIColorCodes)
-
-                                $Deliverables | ForEach-Object -Process {
-
-                                        (Get-Content "$TransferDirectoryPath\$_")  `
-                                                -replace $ANSIColorCodes, '' | 
-                                                        Out-File "$TransferDirectoryPath\$_" -Encoding ASCII 
-
-                                }
-                        }
+                Exit-PSSession
         }
-
 }
-Export-ModuleMember -Function 'Write-ToTransferDirectory'  
+Export-ModuleMember -Function Update-HealthAndStatus
